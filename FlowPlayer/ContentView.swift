374ecf9c59e005ef5dcbcfbbe8376541c6e561a1
Nea <mintbluenea@gmail.com>
2023-03-01 19:52:00 +0100

    Initial Commit

---
+++ FlowPlayer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FlowPlayer
+//
+//  Created by Andrea Garea González on 1/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlowPlayer/FlowPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowPlayerApp.swift
+//  FlowPlayer
+//
+//  Created by Andrea Garea González on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FlowPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
